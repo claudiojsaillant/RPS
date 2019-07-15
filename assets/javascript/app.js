9754959b89c564e55991821150f9d25884e82e58
Cloud <claudiojsaillant@gmail.com>
2019-07-15 12:42:15 -0400

    first feature working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,30 +8,105 @@ var firebaseConfig = {
   appId: "1:233509212002:web:2c66b91bda714330"
 };
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
+var userwins = 0;
+var compwins = 0;
+var p1Choice = '';
+var p2Choice = '';
+var isPlayer1 = false
+var isPlayer2 = false
+var current = '';
+$('#userwins').text(userwins)
+$('#compwins').text(compwins)
+
+database.ref('/player').on("value", function (snap) {
+
+  if (snap.val()) {
+    console.log(snap.val())
+      if (snap.child("p1").exists()) {
+        isPlayer1 = snap.val().p1.isPlayer1;
+        p1Choice = snap.val().p1.p1Choice;
+      } else {
+
+      }
+      if (snap.child("p2").exists()) {
+        isPlayer2 = snap.val().p2.isPlayer2;
+        p2Choice = snap.val().p2.p2Choice;
+      }
+    }
+    if(snap.child("p1").exists() && snap.child("p2").exists()) {
+      game(snap.val().p1.p1Choice + snap.val().p2.p2Choice)
+      database.ref('/player').set({
+
+      });
   
-  
-  // Create a variable to reference the database.
-  var database = firebase.database();
+    }
 
 
-
-$("").click(function(event){
-
-
-  
-  // db push
-database.ref().push({
-  //push stuff
-  dateAdded: firebase.database.ServerValue.TIMESTAMP
-});
-
-});
-
-// Firebase watcher .on("child_added"...
-database.ref().on("child_added", function(snap){
- 
 })
+
+
+$(document).on('click', '.choice', function () {
+  if (!isPlayer1) {
+    database.ref('/player/p1').set({
+       
+        p1Choice: $(this).val(),
+        isPlayer1: true
+      
+    })
+  }
+  else {
+    database.ref('/player/p2').set({
+      
+        p2Choice: $(this).val(),
+        isPlayer2: true
+      
+    })
+
+  }
+
+})
+
+
+function game(finalchoice) {
+
+  $('#userchoise').text(p1Choice)
+  $('#compchoise').text(p2Choice)
+
+  switch (finalchoice) {
+    case 'pr':
+    case 'rs':
+    case 'sp':
+      console.log('You won!');
+      userwins++;
+      $('#desicion').text('You won!');
+      $('#userwins').text(userwins);
+      break;
+    case 'rp':
+    case 'sr':
+    case 'ps':
+      console.log('You lost!');
+      compwins++;
+      $('#desicion').text('Comp wins.');
+      $('#compwins').text(compwins);
+      break;
+    default:
+      console.log('Draw!');
+      $('#desicion').text('Draw!');
+      break;
+  }
+
+}
+
+
+
+
+
+
+
+
+
 
 
