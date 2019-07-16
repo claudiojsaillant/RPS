33cee133c0e18b18e95e3a9dd88a21ec6713a5fe
Cloud <claudiojsaillant@gmail.com>
2019-07-16 14:52:30 -0400

    changes in UI

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,87 +10,148 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
+var childrens = 0;
 var p1wins = 0;
 var p2wins = 0;
-var p1Choice = '';
-var p2Choice = '';
 var isPlayer1 = false;
 var isPlayer2 = false;
+
 $('#userwins').text(p1wins);
 $('#compwins').text(p2wins);
 
-database.ref('/player').on("value", function (snap) {
-  $('#userwins').text(p1wins);
-  $('#compwins').text(p2wins);
-  isPlayer1 = false;
-  isPlayer2 = false;
-  
-  if (snap.child("p1").exists()) {
-    isPlayer1 = snap.val().p1.isPlayer1;
-    p1Choice = snap.val().p1.p1Choice;
-    
-  } else {
-    $('#desicion').text('Waiting for player1...');
+database.ref().on("value", function (snap) {
+
+  if (snap.child("/playerConnection/p1").exists()) {
+    $('#player1').hide();
   }
-  if (snap.child("p2").exists()) {
-    isPlayer2 = snap.val().p2.isPlayer2;
-    p2Choice = snap.val().p2.p2Choice;
-  
+
+  else if (snap.child("/playerConnection/p2").exists()) {
+    $('#player2').hide();
+  }
+
+  if (!snap.child('playerChoice/p1').exists() && snap.child('playerConnection/p2').exists() && !isPlayer1) {
+    $('#row3').hide();
+    $('#status').text('Waiting for P1');
+    $('#row2').show();
+  }
+
+  if (snap.child('playerChoice/p1').exists() && isPlayer2) {
     $('#row3').show();
-  } else if((snap.child("p1").exists())) {
-    $('#desicion').text('Waiting for player2...');
+    $('#row2').hide()
   }
 
-  if (snap.child("p1").exists() && snap.child("p2").exists()) {
-    game(snap.val().p1.p1Choice + snap.val().p2.p2Choice)
-    database.ref('/player').set({
+  if (snap.child('playerChoice/p2').exists() && isPlayer1) {
+    $('#row3').show();
+    $('#row2').hide()
+  }
 
+  if (snap.child("/playerChoice/p2").exists() && snap.child("/playerChoice/p1").exists()) {
+    game(snap.val().playerChoice.p1.p1Choice + snap.val().playerChoice.p2.p2Choice)
+    database.ref('/playerChoice').set({})
+  }
+})
+
+$(document).on('click', '.player', function () {
+  $('#row1').show()
+  $('#row2').show();
+  $('#row3').show();
+  $('#playerselect').hide();
+  if ($(this).val() === 'player1') {
+    isPlayer1 = true;
+    database.ref('/playerConnection/p1').set({
+      isPlayer1: true
+    })
+  }
+  else {
+    isPlayer2 = true;
+    database.ref('/playerConnection/p2').set({
+      isPlayer2: true
     })
   }
 })
 
 $(document).on('click', '.choice', function () {
   if (!isPlayer1) {
-    database.ref('/player/p1').set({
-      p1Choice: $(this).val(),
-      isPlayer1: true
+    database.ref('/playerChoice/p2').set({
+      p2Choice: $(this).attr('value'),
     })
+    $('#status').text('Waiting for P1');
+    $('#row2').show();
     $('#row3').hide()
   }
   else {
-    database.ref('/player/p2').set({
-      p2Choice: $(this).val(),
-      isPlayer2: true
+    database.ref('/playerChoice/p1').set({
+      p1Choice: $(this).attr('value'),
     })
+    $('#status').text('Waiting for P2');
+    $('#row2').show();
+    $('#row3').hide();
   }
 })
 
 function game(finalchoice) {
-  $('#userchoise').text(p1Choice);
-  $('#compchoise').text(p2Choice);
-
   switch (finalchoice) {
     case 'pr':
     case 'rs':
     case 'sp':
-      console.log('P1 Wins!');
+      $('#desicion').text(winLoss(finalchoice, 'w'));
       p1wins++;
+      setTimeout(function () {
+        $('#desicion').text('');
+      }, 1000)
       break;
     case 'rp':
     case 'sr':
     case 'ps':
-      console.log('P2 Wins!');
+      $('#desicion').text(winLoss(finalchoice, 'l'));
+      setTimeout(function () {
+        $('#desicion').text('');
+      }, 1000)
       p2wins++;
       break;
     default:
       console.log('Draw!');
       $('#desicion').text('Draw!');
+      setTimeout(function () {
+        $('#desicion').text('');
+      }, 1000)
       break;
+  }
+  $('#userwins').text(p1wins);
+  $('#compwins').text(p2wins);
+}
+
+$('#reset').on('click', function () {
+  database.ref().set({})
+  location.reload()
+})
+
+function letterFix(letter) {
+  if (letter === 'p') {
+    return 'Paper'
+  }
+  else if (letter === 'r') {
+    return 'Rock'
+  }
+  else {
+    return 'Scissors'
   }
 }
 
+function winLoss(playerContest, action) {
+  choice1 = letterFix(playerContest.charAt(0));
+  choice2 = letterFix(playerContest.charAt(1));
+  if (action === 'w') {
+    return choice1 + ' beats ' + choice2 + '!!  P1 Wins'
+  }
+  else if (action === 'l') {
+    return choice1 + ' beats ' + choice2 + '!!  P2 Wins'
+  }
+}
 
-
+$('#row1').hide()
+$('#row2').hide();
+$('#row3').hide();
 
 
 
